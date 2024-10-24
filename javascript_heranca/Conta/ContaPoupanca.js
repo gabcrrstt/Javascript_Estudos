e06ee6691fa4334766ab46c3ace2d9ac4561338f
Gabo <92454505+gabcrrstt@users.noreply.github.com>
2024-10-24 15:42:41 -0400

    heranca

---
+++ javascript_heranca/Conta/ContaPoupanca.js
@@ -0,0 +1,9 @@
+
+export class ContaPoupanca{
+    constructor(saldoInicial,clinte,agencia){
+        this._saldo = saldoInicial;
+        this._cliente = clinte;
+        this._agencia = agencia;
+
+    }
+}
---
+++ javascript_heranca/index.js
@@ -0,0 +1,17 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./Conta/ContaCorrente.js";
+import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+
+//const cliente2 = new Cliente("Alice", 88822233309);
+
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
+
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+
+console.log(contaPoupanca);
+
+console.log(contaCorrenteRicardo);
